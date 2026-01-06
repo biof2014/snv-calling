5d29166786e39f898003b6dea681b9158e4a3929
David J. H. Shih <djh.shih@gmail.com>
2026-01-06 23:15:14 +0800

    Update evaluation.

--- evaluate-dev.R
+++ evaluate-dev.R
@@ -4,11 +4,18 @@ library(precrec)
 source("R/common.R")
 
 data.haploid <- read_data("data/haploid");
+y <- data.haploid$genotype;
+
 calls.haploid <- qread("calls/haploid.rds");
+
+# confusion matrix based on most probable genotype
+y.hat <- apply(calls.haploid, 1, which.max) - 1;
+table(y.hat, y)
 
 # evaluate calls against ground truth
 scores <- calls.haploid[, 2];
-aucs <- auc(evalmod(scores = scores, labels = data.haploid$genotype));
+ev <- evalmod(scores = scores, labels = y);
+aucs <- auc(ev);
 auroc <- aucs$aucs[aucs$curvetypes == "ROC"];
 
 out <- list(
