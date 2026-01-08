f938518211dcf34db099184421a67396a535fde4
David J. H. Shih <djh.shih@gmail.com>
2026-01-09 00:31:41 +0800

    Add stub for somatic.

---
+++ run-somatic.R
@@ -0,0 +1,8 @@
+source("R/common.R")
+
+data <- read_data("data/somatic");
+
+# FIXME Replace with implementation of somatic model
+calls <- matrix(0, nrow=length(data$genotype), ncol=3);
+write_output(calls, "somatic");
+
