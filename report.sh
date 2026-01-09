f8aca4d4f0b7468c639c7f876da5ad9fb44abfa0
David J. H. Shih <djh.shih@gmail.com>
2026-01-09 19:20:31 +0800

    Make formatting more consistent.

--- report.sh
+++ report.sh
@@ -10,13 +10,13 @@ jq . $json_prod
 
 pass_dev=$(jq .grade.pass[0] $json_dev)
 total_dev=$(jq .grade.total[0] $json_dev)
-echo "Development score: $pass_dev / $total_dev"
+echo "Development score: $pass_dev/$total_dev"
 
 pass_prod=$(jq .grade.pass[0] $json_prod)
 total_prod=$(jq .grade.total[0] $json_prod)
 pass_prod=$(( 2 * pass_prod))
 total_prod=$(( 2 * total_prod))
-echo "Production score: $pass_prod / $total_prod"
+echo "Production score: $pass_prod/$total_prod"
 
 pass=$(( pass_dev + pass_prod ))
 total=$(( total_dev + total_prod ))
