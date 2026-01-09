a38b0b3c912caba161a6968fa188ee47855fab1c
David J. H. Shih <djh.shih@gmail.com>
2026-01-09 19:25:07 +0800

    Clean up output.

--- report.sh
+++ report.sh
@@ -3,20 +3,15 @@
 json_dev="evaluate.json"
 json_prod="prod/evaluate.json"
 
-echo "development:"
-jq . $json_dev
-echo "production:"
-jq . $json_prod
-
 pass_dev=$(jq .grade.pass[0] $json_dev)
 total_dev=$(jq .grade.total[0] $json_dev)
-echo "Development score: $pass_dev/$total_dev"
+echo "Development points: $pass_dev/$total_dev"
 
 pass_prod=$(jq .grade.pass[0] $json_prod)
 total_prod=$(jq .grade.total[0] $json_prod)
 pass_prod=$(( 2 * pass_prod))
 total_prod=$(( 2 * total_prod))
-echo "Production score: $pass_prod/$total_prod"
+echo "Production points: $pass_prod/$total_prod"
 
 pass=$(( pass_dev + pass_prod ))
 total=$(( total_dev + total_prod ))
