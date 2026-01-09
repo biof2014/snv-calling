63dff98d4643eb43fac8bcac0be002ea18443d85
David J. H. Shih <djh.shih@gmail.com>
2026-01-09 18:35:28 +0800

    Setup production environment.

---
+++ setup-prod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Evaluate models on production data
+
+set -euo pipefail
+IFS=$'\n\t'
+
+if (( $# < 1 )); then
+	echo "usage: ${0##*/} <PROD_KEY>"
+	exit 1
+fi
+
+key=$1
+
+# get new copy of template repo
+rm -rf prod
+git clone --depth=1 https://github.com/biof2014/snv-calling prod
+
+# get production data
+(
+cd prod
+rm -rf data
+git clone --depth=1 https://${key}@github.com/biof2014/snv-calling-prod data
+)
+
+# copy over models
+cp run-diploid.R run-somatic.R prod/
+
