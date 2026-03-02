6e1b2dfee9bbed9f43097113300d6c56c8cfc51c
David J. H. Shih <djh.shih@gmail.com>
2026-03-02 14:46:38 +0800

    Update rip version.

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 if [[ ! -f ./bin/rip ]]; then
 	mkdir -p bin
 	cd bin
-	curl -L https://github.com/djhshih/rip/archive/v0.3.tar.gz |
+	curl -L https://github.com/djhshih/rip/archive/v0.5.tar.gz |
 		tar --strip-components=1 -xz
 fi
 )
